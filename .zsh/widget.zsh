31f0d452c526c4b84f757dc9b31990d22fc38329
nakatuba <nakatuba0626@gmail.com>
2021-05-18 22:20:17 +0900

    Remove double quotation

--- .zsh/widget.zsh
+++ .zsh/widget.zsh
@@ -1,5 +1,5 @@
 fzf-cdr-widget() {
-  local dir="$(cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf --height 40% --reverse)"
+  local dir=$(cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf --height 40% --reverse)
   if [ -n "$dir" ]; then
     BUFFER="cd $dir"
     zle accept-line
@@ -10,7 +10,7 @@ zle     -N   fzf-cdr-widget
 bindkey '^]' fzf-cdr-widget
 
 fzf-ghq-widget() {
-  local dir="$(ghq list | fzf --height 40% --reverse)"
+  local dir=$(ghq list | fzf --height 40% --reverse)
   if [ -n "$dir" ]; then
     BUFFER="cd $(ghq root)/$dir"
     zle accept-line
@@ -21,7 +21,7 @@ zle     -N   fzf-ghq-widget
 bindkey '^g^g' fzf-ghq-widget
 
 fzf-git-checkout-widget() {
-  local branch="$(_gb)"
+  local branch=$(_gb)
   if [ -n "$branch" ]; then
     BUFFER="git checkout $branch"
     zle accept-line
