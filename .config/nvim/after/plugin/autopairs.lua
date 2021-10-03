7a260c62a345aa8722a415967998f18343b558fa
nakatuba <nakatuba0626@gmail.com>
2021-10-04 02:12:19 +0900

    Add nvim-autopairs rule to add spaces between parentheses

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -1,7 +1,7 @@
+require('nvim-autopairs').setup()
+
 local remap = vim.api.nvim_set_keymap
 local npairs = require('nvim-autopairs')
-
-npairs.setup()
 
 -- skip it, if you use another global object
 _G.MUtils= {}
@@ -16,3 +16,31 @@ end
 
 
 remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+
+local Rule   = require('nvim-autopairs.rule')
+
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']')
+}
