583907a2c0f5a3939990ef473c4db5ad1f5f893c
nakatuba <nakatuba0626@gmail.com>
2021-10-05 04:46:27 +0900

    Enable autotag for html

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,8 +1,8 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { 'python', 'vue' },
+  ensure_installed = { 'python', 'html', 'vue' },
   highlight = {
     enable = true,
-    disable = { 'vue' }
+    disable = { 'html', 'vue' }
   },
   indent = {
     enable = true
