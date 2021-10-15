91be557beca89a24b72c5a55a7168bceb5b35923
nakatuba <nakatuba0626@gmail.com>
2021-10-16 04:24:04 +0900

    Fix nvim-remote alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,5 +1,5 @@
 if command -v nvim > /dev/null; then
   alias vi='nvim'
   alias vim='nvim'
-  [ -n "$VIM" ] && alias nvim="nvr -cc 'split | only' -c 'set number'"
+  [ -n "$VIM" ] && alias nvim='nvr -l'
 fi
