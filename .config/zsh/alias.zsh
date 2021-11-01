a22e6de85820ed8358f54332d6c46e968f916c1d
nakatuba <nakatuba0626@gmail.com>
2021-11-02 03:36:10 +0900

    Remove some exa aliases

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -5,10 +5,8 @@ if command -v nvim > /dev/null; then
 fi
 
 if command -v exa > /dev/null; then
-  alias ls='exa --icons'
   alias ll='exa --icons -l'
   alias la='exa --icons -la'
-  alias tree='exa --icons --tree'
 fi
 
 command -v bat > /dev/null && alias cat='bat --paging=never'
