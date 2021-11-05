c2401ec9fd1c13e25586eb0575d8bb0370e1a323
nakatuba <nakatuba0626@gmail.com>
2021-11-05 22:39:35 +0900

    Resolve lspsaga incompatibility with neovim 0.5.1

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -3,11 +3,14 @@ local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
 
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>',      opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+
+  vim.api.nvim_command [[autocmd CursorHoldI <buffer> lua vim.lsp.buf.signature_help()]]
+  vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 
   require('lspsaga').init_lsp_saga {
     rename_action_keys = {
@@ -18,9 +21,6 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<leader>rn', "<cmd>lua require('lspsaga.rename').rename()<CR>",                       opts)
   buf_set_keymap('n', '[e',         "<cmd>lua require('lspsaga.diagnostic').lsp_jump_diagnostic_prev()<CR>", opts)
   buf_set_keymap('n', ']e',         "<cmd>lua require('lspsaga.diagnostic').lsp_jump_diagnostic_next()<CR>", opts)
-
-  vim.api.nvim_command [[autocmd CursorHoldI <buffer> lua require('lspsaga.signaturehelp').signature_help()]]
-  vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 end
 
 nvim_lsp.pyright.setup {
