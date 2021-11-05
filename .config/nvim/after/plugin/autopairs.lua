ed1fb88572ea79cbad2ce790fcbf1d78240521d0
nakatuba <nakatuba0626@gmail.com>
2021-11-06 00:15:39 +0900

    Support nvim-autopairs update

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -1,21 +1,10 @@
-require('nvim-autopairs').setup()
+require('nvim-autopairs').setup {
+  map_cr = true
+}
 
-local remap = vim.api.nvim_set_keymap
+vim.api.nvim_set_keymap('i' , '<C-h>', 'v:lua.MPairs.autopairs_bs()', { expr = true, noremap = true })
+
 local npairs = require('nvim-autopairs')
-
-_G.MUtils = {}
-
-MUtils.completion_confirm = function()
-  if vim.fn.pumvisible() ~= 0 then
-    return npairs.esc("<cr>")
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
-remap('i' , '<CR>', 'v:lua.MUtils.completion_confirm()', { expr = true, noremap = true })
-remap('i' , '<C-h>', 'v:lua.MPairs.autopairs_bs()', { expr = true, noremap = true })
-
 local Rule = require('nvim-autopairs.rule')
 
 npairs.add_rules {
