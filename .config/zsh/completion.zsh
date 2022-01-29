be77f4166fb67806af19b1fd53f74aa2f9b0146a
nakatuba <nakatuba0626@gmail.com>
2022-01-30 05:01:34 +0900

    Add homebrew completion

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,1 +1,11 @@
+# pip
 command -v pip > /dev/null && eval "$(pip completion --zsh)"
+
+# brew
+if type brew &>/dev/null
+then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
+  autoload -Uz compinit
+  compinit
+fi
