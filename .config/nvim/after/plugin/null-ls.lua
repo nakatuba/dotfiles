d384720df6e63e13b1802a139084613828743888
nakatuba <nakatuba0626@gmail.com>
2022-02-08 02:13:28 +0900

    Install null-ls.nvim

---
+++ .config/nvim/after/plugin/null-ls.lua
@@ -0,0 +1,10 @@
+require('null-ls').setup {
+  diagnostics_format = '[#{c}] #{m} (#{s})',
+  sources = {
+    require('null-ls').builtins.diagnostics.flake8.with {
+      extra_args = { '--ignore=E501,W503' }
+    },
+    require('null-ls').builtins.formatting.black,
+    require('null-ls').builtins.formatting.isort
+  }
+}
