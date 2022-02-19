26bdb1c17179bb727cc12c3194735fdc0648b0ba
nakatuba <nakatuba0626@gmail.com>
2022-02-19 16:00:13 +0900

    Add mappings to jump to next (prev) diagnostic

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -1,13 +1,9 @@
-local nvim_lsp = require('lspconfig')
-
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
   local opts = { noremap = true, silent = true }
 
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>',      opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>',      opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
   vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 
@@ -20,14 +16,16 @@ local on_attach = function(client, bufnr)
     }
   }
 
-  buf_set_keymap('n', '<leader>ca', "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", opts)
-  buf_set_keymap('n', '<leader>rn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 
-  vim.api.nvim_command [[autocmd CursorHoldI <buffer> lua require('lspsaga.signaturehelp').signature_help()]]
-  vim.api.nvim_command [[autocmd CursorHold  <buffer> lua require('lspsaga.diagnostic').show_cursor_diagnostics()]]
+  vim.api.nvim_command [[autocmd CursorHoldI <buffer> Lspsaga signature_help]]
+  vim.api.nvim_command [[autocmd CursorHold  <buffer> Lspsaga show_cursor_diagnostics]]
 end
 
-nvim_lsp.pyright.setup {
+require('lspconfig').pyright.setup {
   on_attach = on_attach,
   settings = {
     python = {
@@ -38,15 +36,15 @@ nvim_lsp.pyright.setup {
     }
   },
   before_init = function(_, config)
-    config.settings.python.pythonPath = nvim_lsp.util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+    config.settings.python.pythonPath = require('lspconfig').util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
   end
 }
 
-nvim_lsp.gopls.setup {
+require('lspconfig').gopls.setup {
   on_attach = on_attach
 }
 
-nvim_lsp.vuels.setup {
+require('lspconfig').vuels.setup {
   on_attach = on_attach,
   settings = {
     vetur = {
