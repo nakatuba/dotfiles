04877711424e546a909c3568bb09b044ce27595f
nakatuba <nakatuba0626@gmail.com>
2022-03-05 23:51:19 +0900

    Change gitsigns.nvim mappings

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,19 +1,31 @@
 require('gitsigns').setup {
-  keymaps = {
-    noremap = true,
+  on_attach = function(bufnr)
+    local function map(mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
 
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'" },
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'" },
+    -- Navigation
+    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
 
-    ['n ghs'] = '<cmd>Gitsigns stage_hunk<CR>',
-    ['v ghs'] = '<cmd>Gitsigns stage_hunk<CR>',
-    ['n ghr'] = '<cmd>Gitsigns reset_hunk<CR>',
-    ['v ghr'] = '<cmd>Gitsigns reset_hunk<CR>',
-    ['n ghS'] = '<cmd>Gitsigns stage_buffer<CR>',
-    ['n ghu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-    ['n ghR'] = '<cmd>Gitsigns reset_buffer<CR>',
-    ['n ghp'] = '<cmd>Gitsigns preview_hunk<CR>',
-    ['n ghb'] = '<cmd>Gitsigns blame_line<CR>',
-    ['n ghd'] = '<cmd>Gitsigns diffthis<CR>'
-  }
+    -- Actions
+    map('n', 'ghs', ':Gitsigns stage_hunk<CR>')
+    map('v', 'ghs', ':Gitsigns stage_hunk<CR>')
+    map('n', 'ghr', ':Gitsigns reset_hunk<CR>')
+    map('v', 'ghr', ':Gitsigns reset_hunk<CR>')
+    map('n', 'ghS', '<cmd>Gitsigns stage_buffer<CR>')
+    map('n', 'ghu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+    map('n', 'ghR', '<cmd>Gitsigns reset_buffer<CR>')
+    map('n', 'ghp', '<cmd>Gitsigns preview_hunk<CR>')
+    map('n', 'ghb', '<cmd>lua require("gitsigns").blame_line { full = true }<CR>')
+    map('n', 'gtb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+    map('n', 'ghd', '<cmd>Gitsigns diffthis<CR>')
+    map('n', 'ghD', '<cmd>lua require("gitsigns").diffthis("~")<CR>')
+    map('n', 'gtd', '<cmd>Gitsigns toggle_deleted<CR>')
+
+    -- Text object
+    map('o', 'ih', ':<C-u>Gitsigns select_hunk<CR>')
+    map('x', 'ih', ':<C-u>Gitsigns select_hunk<CR>')
+  end
 }
