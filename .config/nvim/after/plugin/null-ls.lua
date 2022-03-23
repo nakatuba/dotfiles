80f34326473a76a9d43228ffe2c6a83f3080f2fa
nakatuba <nakatuba0626@gmail.com>
2022-03-23 11:55:18 +0900

    Add eslint_d to null-ls sources

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -1,6 +1,7 @@
 require('null-ls').setup {
   diagnostics_format = '[#{c}] #{m} (#{s})',
   sources = {
+    require('null-ls').builtins.diagnostics.eslint_d,
     require('null-ls').builtins.diagnostics.flake8.with {
       extra_args = { '--ignore=E501,W503' }
     },
