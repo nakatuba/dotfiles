11b33efe7af17ff500efbdd54b804b8cda75183b
nakatuba <nakatuba0626@gmail.com>
2022-05-09 09:42:59 +0900

    Enable incline.nvim

--- .config/nvim/after/plugin/incline.lua
+++ .config/nvim/after/plugin/incline.lua
@@ -4,7 +4,3 @@ require('incline').setup {
     return vim.fn.fnamemodify(bufname, ':.')
   end
 }
-
-require('incline').disable()
-
-vim.api.nvim_set_keymap('n' , '<leader>i', '<cmd>lua require("incline").toggle()<CR>', { noremap = true, silent = true })
