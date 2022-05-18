bfe50c55c0293e55492b21b55bfd6645f9334491
nakatuba <nakatuba0626@gmail.com>
2022-05-19 04:28:59 +0900

    Update gitsigns config

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,29 +1,38 @@
 require('gitsigns').setup {
   on_attach = function(bufnr)
-    local function map(mode, lhs, rhs, opts)
-        opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
-        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
     end
 
     -- Navigation
-    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
 
     -- Actions
-    map('n', 'ghs', ':Gitsigns stage_hunk<CR>')
-    map('v', 'ghs', ':Gitsigns stage_hunk<CR>')
-    map('n', 'ghr', ':Gitsigns reset_hunk<CR>')
-    map('v', 'ghr', ':Gitsigns reset_hunk<CR>')
-    map('n', 'ghS', '<cmd>Gitsigns stage_buffer<CR>')
-    map('n', 'ghu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-    map('n', 'ghR', '<cmd>Gitsigns reset_buffer<CR>')
-    map('n', 'ghp', '<cmd>Gitsigns preview_hunk<CR>')
-    map('n', 'ghb', '<cmd>lua require("gitsigns").blame_line { full = true }<CR>')
-    map('n', 'ghd', '<cmd>Gitsigns diffthis<CR>')
-    map('n', 'ghD', '<cmd>lua require("gitsigns").diffthis("~")<CR>')
+    map({'n', 'v'}, 'ghs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, 'ghr', ':Gitsigns reset_hunk<CR>')
+    map('n', 'ghS', gs.stage_buffer)
+    map('n', 'ghu', gs.undo_stage_hunk)
+    map('n', 'ghR', gs.reset_buffer)
+    map('n', 'ghp', gs.preview_hunk)
+    map('n', 'ghb', function() gs.blame_line { full = true } end)
+    map('n', 'ghd', gs.diffthis)
+    map('n', 'ghD', function() gs.diffthis('~') end)
 
     -- Text object
-    map('o', 'ih', ':<C-u>Gitsigns select_hunk<CR>')
-    map('x', 'ih', ':<C-u>Gitsigns select_hunk<CR>')
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
