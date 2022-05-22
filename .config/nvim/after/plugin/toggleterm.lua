bd97fa01a330f667acfaa6349d07c3a79ce54ebc
nakatuba <nakatuba0626@gmail.com>
2022-05-22 18:01:27 +0900

    Change toggleterm direction dynamically

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -1,5 +1,17 @@
 require('toggleterm').setup {
-  open_mapping = '<C-Space>',
+  size = function(term)
+    if term.direction == 'horizontal' then
+      return vim.o.lines * 0.5
+    elseif term.direction == 'vertical' then
+      return vim.o.columns * 0.5
+    end
+  end,
   persist_size = false,
-  direction = 'horizontal'
 }
+
+vim.keymap.set('n', '<C-Space>', function()
+  local direction = vim.o.columns < vim.o.lines * 2 and 'horizontal' or 'vertical'
+  vim.api.nvim_command('ToggleTerm direction=' .. direction)
+end)
+
+vim.keymap.set('t', '<C-Space>', '<cmd>ToggleTerm<CR>')
