5187cf79fca2c5633f01b6728199656cba625e0d
nakatuba <nakatuba0626@gmail.com>
2022-06-04 00:23:42 +0900

    Add nvim-cmp `/` setup

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -1,6 +1,6 @@
 local cmp = require('cmp')
 
-cmp.setup {
+cmp.setup({
   snippet = {
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body)
@@ -23,7 +23,14 @@ cmp.setup {
     { name = 'buffer' },
     { name = 'path' }
   }
-}
+})
+
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
