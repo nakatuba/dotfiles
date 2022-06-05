308496ba4b83332d4d8760bf2ec4b924e1460c36
nakatuba <nakatuba0626@gmail.com>
2022-06-05 21:44:29 +0900

    Update incline.nvim config

--- .config/nvim/after/plugin/incline.lua
+++ .config/nvim/after/plugin/incline.lua
@@ -1,6 +1,10 @@
 require('incline').setup {
   render = function(props)
     local bufname = vim.api.nvim_buf_get_name(props.buf)
-    return vim.fn.fnamemodify(bufname, ':.')
+    local res = vim.fn.fnamemodify(bufname, ':.')
+    if vim.api.nvim_buf_get_option(props.buf, 'modified') then
+      res = res .. ' [+]'
+    end
+    return res
   end
 }
