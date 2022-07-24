883dc03276afece546832c83e3ec5c1fed4bd3da
nakatuba <nakatuba0626@gmail.com>
2022-07-25 06:16:34 +0900

    Add trouble.nvim diagnostics mappings

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -3,11 +3,13 @@ if not pcall(require, 'lspconfig') then return end
 local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true }
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',         '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',         '<cmd>TroubleToggle lsp_references<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>Lspsaga code_action<CR>',          opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',          '<cmd>Lspsaga hover_doc<CR>',            opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>Lspsaga rename<CR>',               opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',         '<cmd>lua vim.lsp.buf.definition()<CR>',                    opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',         '<cmd>TroubleToggle lsp_references<CR>',                    opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>Lspsaga code_action<CR>',                             opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',          '<cmd>Lspsaga hover_doc<CR>',                               opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>Lspsaga rename<CR>',                                  opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D',  '<cmd>TroubleToggle workspace_diagnostics<CR>',             opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d',  '<cmd>TroubleToggle document_diagnostics<CR>',              opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d',         '<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d',         '<cmd>lua vim.diagnostic.goto_next({ float = false })<CR>', opts)
 
