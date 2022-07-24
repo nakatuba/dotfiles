9c4e383c288552be93075e0b2aa14cf55f98123c
nakatuba <nakatuba0626@gmail.com>
2022-07-25 05:54:41 +0900

    Change substitute.nvim mappings

--- .config/nvim/after/plugin/substitute.lua
+++ .config/nvim/after/plugin/substitute.lua
@@ -1,6 +1,6 @@
 if not pcall(require, 'substitute') then return end
 
-vim.keymap.set('n', '<leader>s',  "<cmd>lua require('substitute').operator()<cr>")
-vim.keymap.set('n', '<leader>ss', "<cmd>lua require('substitute').line()<cr>")
-vim.keymap.set('n', '<leader>S',  "<cmd>lua require('substitute').eol()<cr>")
-vim.keymap.set('v', '<leader>s',  "<cmd>lua require('substitute').visual()<cr>")
+vim.keymap.set('n', 'gs',  "<cmd>lua require('substitute').operator()<cr>")
+vim.keymap.set('n', 'gss', "<cmd>lua require('substitute').line()<cr>")
+vim.keymap.set('n', 'gS',  "<cmd>lua require('substitute').eol()<cr>")
+vim.keymap.set('v', 'gs',  "<cmd>lua require('substitute').visual()<cr>")
