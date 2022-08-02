75ae089fa4c12c7611de6248c2c1def06dbc9bec
nakatuba <nakatuba0626@gmail.com>
2022-08-02 23:36:07 +0900

    Update python linter and formatter configs

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -6,9 +6,20 @@ require('null-ls').setup {
     require('null-ls').builtins.diagnostics.eslint,
     require('null-ls').builtins.formatting.prettier,
     require('null-ls').builtins.diagnostics.flake8.with {
-      extra_args = { '--ignore=E501,W503' }
+      extra_args = { '--ignore=E501,W503' },
+      cwd = function(params)
+        return vim.fn.fnamemodify(params.bufname, ':h')
+      end
     },
-    require('null-ls').builtins.formatting.black,
-    require('null-ls').builtins.formatting.isort
+    require('null-ls').builtins.formatting.black.with {
+      cwd = function(params)
+        return vim.fn.fnamemodify(params.bufname, ':h')
+      end
+    },
+    require('null-ls').builtins.formatting.isort.with {
+      cwd = function(params)
+        return vim.fn.fnamemodify(params.bufname, ':h')
+      end
+    }
   }
 }
