36a977abb894c4eb615173fef30cfd8553055c28
nakatuba <nakatuba0626@gmail.com>
2022-08-07 05:31:13 +0900

    Solve lspsaga error

--- .config/nvim/after/plugin/lspsaga.lua
+++ .config/nvim/after/plugin/lspsaga.lua
@@ -1,6 +1,9 @@
 if not pcall(require, 'lspsaga') then return end
 
 require('lspsaga').init_lsp_saga {
+  code_action_lightbulb = {
+    enable = false
+  },
   code_action_keys = {
     quit = '<Esc>'
   },
