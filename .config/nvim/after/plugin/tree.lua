459df321eaf71525e6746cdf2f6529736514422c
nakatuba <nakatuba0626@gmail.com>
2022-08-19 17:43:00 +0900

    Revert "Disable nvim-tree.lua window picker"
    This reverts commit 313a539bd7d3cc7c1ab22784f8cba7874d0a3df1.

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -15,13 +15,6 @@ require('nvim-tree').setup {
   },
   update_focused_file = {
     enable = true
-  },
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false
-      }
-    }
   }
 }
 
