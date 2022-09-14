31d29060955ef29e12abf4a742cddc5f645177d0
nakatuba <nakatuba0626@gmail.com>
2022-09-15 02:57:25 +0900

    Fix statusline maximize status to render immediately

--- .config/nvim/after/plugin/maximize.lua
+++ .config/nvim/after/plugin/maximize.lua
@@ -1,3 +1,6 @@
 if not pcall(require, 'maximize') then return end
 
-vim.keymap.set('n', '<CR>', "<cmd>lua require('maximize').toggle()<CR>")
+vim.keymap.set('n', '<CR>', function ()
+  require('maximize').toggle()
+  require('lualine').refresh()
+end)
