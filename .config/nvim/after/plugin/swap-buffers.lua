c45b54a30f4171bd92535603f8bd18f2195279ef
nakatuba <nakatuba0626@gmail.com>
2022-09-28 02:04:13 +0900

    Change swap-buffers.nvim mappings

--- .config/nvim/after/plugin/swap-buffers.lua
+++ .config/nvim/after/plugin/swap-buffers.lua
@@ -1,6 +1,6 @@
 if not pcall(require, 'swap-buffers') then return end
 
-vim.keymap.set('n', '<C-w>H', "<cmd>lua require('swap-buffers').swap_buffers('h')<CR>")
-vim.keymap.set('n', '<C-w>J', "<cmd>lua require('swap-buffers').swap_buffers('j')<CR>")
-vim.keymap.set('n', '<C-w>K', "<cmd>lua require('swap-buffers').swap_buffers('k')<CR>")
-vim.keymap.set('n', '<C-w>L', "<cmd>lua require('swap-buffers').swap_buffers('l')<CR>")
+vim.keymap.set('n', '<M-h>', "<cmd>lua require('swap-buffers').swap_buffers('h')<CR>")
+vim.keymap.set('n', '<M-j>', "<cmd>lua require('swap-buffers').swap_buffers('j')<CR>")
+vim.keymap.set('n', '<M-k>', "<cmd>lua require('swap-buffers').swap_buffers('k')<CR>")
+vim.keymap.set('n', '<M-l>', "<cmd>lua require('swap-buffers').swap_buffers('l')<CR>")
