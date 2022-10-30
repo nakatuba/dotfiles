cc8b6ffaa7bc30fa34aae01419d523f78bfca6b7
nakatuba <nakatuba0626@gmail.com>
2022-10-30 13:15:42 +0900

    Revert "Add visual mode to HopWord mapping"
    This reverts commit a8a5508477078683eb161183533e938e4e656b98.

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -2,4 +2,4 @@ if not pcall(require, 'hop') then return end
 
 require('hop').setup()
 
-vim.keymap.set({'n', 'v'}, '<leader>w', '<cmd>HopWord<CR>')
+vim.keymap.set('n', '<leader>w', '<cmd>HopWord<CR>')
