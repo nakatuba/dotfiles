3ab572dad8453ff82dfb6a506f8a7a59331958f5
nakatuba <nakatuba0626@gmail.com>
2022-11-12 01:44:24 +0900

    Add fzf-git bindkeys

--- .config/zsh/fzf-git.zsh
+++ .config/zsh/fzf-git.zsh
@@ -66,5 +66,6 @@ join-lines() {
     eval "fzf-g$c-widget() { local result=\$(_g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
     eval "zle -N fzf-g$c-widget"
     eval "bindkey '^g^$c' fzf-g$c-widget"
+    eval "bindkey '^g$c' fzf-g$c-widget"
   done
 } f b t r h s
