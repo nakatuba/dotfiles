1b1a9a07eb532022320cb73f6c9252c1f1b87ace
nakatuba <nakatuba0626@gmail.com>
2022-11-26 01:55:40 +0900

    Improve fzf git files

--- .config/zsh/fzf-git.zsh
+++ .config/zsh/fzf-git.zsh
@@ -10,7 +10,7 @@ _gf() {
   is_in_git_repo || return
   git -c color.status=always status --short |
   fzf-down -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always -- {-1} | sed 1,4d)' |
+    --preview '(if git ls-files --error-unmatch {-1} > /dev/null 2>&1; then git diff --color=always -- {-1} | sed 1,4d; else bat -n --color=always {-1}; fi)' |
   cut -c4- | sed 's/.* -> //'
 }
 
