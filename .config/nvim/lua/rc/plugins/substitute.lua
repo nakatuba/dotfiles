4c4c155abc2a6bae584567bcc7ef91660d0e1506
nakatuba <nakatuba0626@gmail.com>
2023-02-16 09:03:37 +0900

    Resolve compatibility between substitute.nvim and tmux.nvim

--- .config/nvim/lua/rc/plugins/substitute.lua
+++ .config/nvim/lua/rc/plugins/substitute.lua
@@ -1,9 +1,29 @@
 require('substitute').setup()
 
-vim.keymap.set('n', 'gs',  "<cmd>lua require('substitute').operator()<CR>")
-vim.keymap.set('n', 'gss', "<cmd>lua require('substitute').line()<CR>")
-vim.keymap.set('n', 'gS',  "<cmd>lua require('substitute').eol()<CR>")
-vim.keymap.set('x', 'gs',  "<cmd>lua require('substitute').visual()<CR>")
+vim.keymap.set('n', 'gs', function ()
+  if vim.env.TMUX then
+    require('tmux').sync_registers()
+  end
+  require('substitute').operator()
+end)
+vim.keymap.set('n', 'gss', function ()
+  if vim.env.TMUX then
+    require('tmux').sync_registers()
+  end
+  require('substitute').line()
+end)
+vim.keymap.set('n', 'gS', function ()
+  if vim.env.TMUX then
+    require('tmux').sync_registers()
+  end
+  require('substitute').eol()
+end)
+vim.keymap.set('x', 'gs', function ()
+  if vim.env.TMUX then
+    require('tmux').sync_registers()
+  end
+  require('substitute').visual()
+end)
 
 vim.keymap.set('n', 'cx',  "<cmd>lua require('substitute.exchange').operator()<CR>")
 vim.keymap.set('n', 'cxx', "<cmd>lua require('substitute.exchange').line()<CR>")
