ba77732a3f63b2372d03471cda63e2f37ffef730
nakatuba <nakatuba0626@gmail.com>
2023-03-26 08:49:38 +0900

    Add terraform completion

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,6 +1,12 @@
+# pip
 command -v pip > /dev/null && eval "$(pip completion --zsh)"
 
+# brew
 command -v brew > /dev/null && FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
 
 autoload -Uz compinit
 compinit
+
+# terraform
+autoload -U +X bashcompinit && bashcompinit
+complete -o nospace -C /opt/homebrew/bin/terraform terraform
