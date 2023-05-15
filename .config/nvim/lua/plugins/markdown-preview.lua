f691ac8b2c7c1a5695d897651f17b32e854794fe
nakatuba <nakatuba0626@gmail.com>
2023-05-15 15:38:21 +0900

    Add markdown-preview.lua mapping

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,13 @@
 return {
   'iamcco/markdown-preview.nvim',
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'markdown',
+      callback = function()
+        vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<CR>', { buffer = true })
+      end
+    })
+  end,
   build = function()
     vim.fn['mkdp#util#install']()
   end,
