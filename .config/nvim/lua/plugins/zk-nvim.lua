588a70ee5a056a5207e96fe420d39a702d6ffcbf
nakatuba <nakatuba0626@gmail.com>
2023-05-22 23:12:39 +0900

    Revert "Add picker mapping to delete notes"
    This reverts commit 487dcb10d4b34b4846945324fd589545b3569cf3.

--- .config/nvim/lua/plugins/zk-nvim.lua
+++ .config/nvim/lua/plugins/zk-nvim.lua
@@ -14,24 +14,7 @@ return {
     end)
 
     vim.keymap.set('n', '<leader>nn', '<cmd>ZkNew<CR>')
-    vim.keymap.set('n', '<leader>no', function()
-      require('zk').edit(nil, {
-        telescope = {
-          attach_mappings = function(_, map)
-            map('i', '<C-d>', function(prompt_bufnr)
-              local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
-              if vim.fn.input('Delete selected notes? [y/N]: ') == 'y' then
-                picker:delete_selection(function(selection)
-                  return vim.fn.delete(selection.path) == 0
-                end)
-                vim.fn.system('zk index')
-              end
-            end)
-            return true
-          end
-        }
-      })
-    end)
+    vim.keymap.set('n', '<leader>no', '<cmd>ZkNotes<CR>')
     vim.keymap.set('n', '<leader>nc', '<cmd>ZkClean<CR>')
     vim.keymap.set('n', '<leader>ns', '<cmd>ZkSave<CR>')
   end
