d2fb9c72455d349902bfe79bf40e8117de6a7b62
nakatuba <nakatuba0626@gmail.com>
2023-05-23 20:39:06 +0900

    Remove zk clean

--- .config/nvim/lua/plugins/zk-nvim.lua
+++ .config/nvim/lua/plugins/zk-nvim.lua
@@ -5,17 +5,12 @@ return {
       picker = 'telescope'
     }
 
-    require('zk.commands').add('ZkClean', function()
-      vim.fn.system('zk clean')
-    end)
-
     require('zk.commands').add('ZkSave', function()
       vim.fn.system('zk save')
     end)
 
     vim.keymap.set('n', '<leader>nn', '<cmd>ZkNew<CR>')
     vim.keymap.set('n', '<leader>no', '<cmd>ZkNotes<CR>')
-    vim.keymap.set('n', '<leader>nc', '<cmd>ZkClean<CR>')
     vim.keymap.set('n', '<leader>ns', '<cmd>ZkSave<CR>')
   end
 }
