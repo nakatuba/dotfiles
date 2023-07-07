dc247dc5e6f9ef27ab31d554a38ac976889b5c38
nakatuba <nakatuba0626@gmail.com>
2023-07-08 03:44:14 +0900

    Revert "Install vim-table-mode"
    This reverts commit 8d6d90fed87181db03432c65a04e35a440a51fd8.

---
+++ .config/nvim/lua/plugins/mkdnflow.lua
@@ -0,0 +1,21 @@
+return {
+  'jakewvincent/mkdnflow.nvim',
+  config = function()
+    require('mkdnflow').setup {
+      modules = {
+        maps = false
+      }
+    }
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'markdown',
+      callback = function()
+        vim.keymap.set('n', '<leader>ir', '<cmd>MkdnTableNewRowBelow<CR>',  { buffer = true })
+        vim.keymap.set('n', '<leader>iR', '<cmd>MkdnTableNewRowAbove<CR>',  { buffer = true })
+        vim.keymap.set('n', '<leader>ic', '<cmd>MkdnTableNewColAfter<CR>',  { buffer = true })
+        vim.keymap.set('n', '<leader>iC', '<cmd>MkdnTableNewColBefore<CR>', { buffer = true })
+      end
+    })
+  end,
+  ft = { 'markdown' }
+}
