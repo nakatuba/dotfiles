78fdfa6cf34413a5d9f90d98f389deaf398e0da0
nakatuba <nakatuba0626@gmail.com>
2023-07-13 07:42:30 +0900

    Uninstall bullets.vim

--- .config/nvim/lua/plugins/mkdnflow.lua
+++ .config/nvim/lua/plugins/mkdnflow.lua
@@ -10,10 +10,23 @@ return {
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'markdown',
       callback = function()
+        vim.keymap.set('i', '<CR>', '<cmd>MkdnEnter<CR>', { buffer = true })
+
+        vim.keymap.set('n', 'o', '<cmd>MkdnNewListItemBelowInsert<CR>', { buffer = true })
+        vim.keymap.set('n', 'O', '<cmd>MkdnNewListItemAboveInsert<CR>', { buffer = true })
+
         vim.keymap.set('n', '<leader>ir', '<cmd>MkdnTableNewRowBelow<CR>',  { buffer = true })
         vim.keymap.set('n', '<leader>iR', '<cmd>MkdnTableNewRowAbove<CR>',  { buffer = true })
         vim.keymap.set('n', '<leader>ic', '<cmd>MkdnTableNewColAfter<CR>',  { buffer = true })
         vim.keymap.set('n', '<leader>iC', '<cmd>MkdnTableNewColBefore<CR>', { buffer = true })
+
+        vim.keymap.set({'i', 's'}, '<Tab>', function()
+          return vim.fn['vsnip#available'](1) == 1 and '<Plug>(vsnip-expand-or-jump)' or '<cmd>MkdnTab<CR>'
+        end, { buffer = true, expr = true })
+
+        vim.keymap.set({'i', 's'}, '<S-Tab>', function()
+          return vim.fn['vsnip#jumpable'](-1) == 1 and '<Plug>(vsnip-jump-prev)' or '<cmd>MkdnSTab<CR>'
+        end, { buffer = true, expr = true })
       end
     })
   end,
