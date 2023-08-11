ba453d7b5d37e47ec3c04a91445b8e14e60546ec
nakatuba <nakatuba0626@gmail.com>
2023-08-12 07:23:08 +0900

    Remove MkdnTab mappings

--- .config/nvim/lua/plugins/mkdnflow.lua
+++ .config/nvim/lua/plugins/mkdnflow.lua
@@ -19,14 +19,6 @@ return {
         vim.keymap.set('n', '<leader>iR', '<cmd>MkdnTableNewRowAbove<CR>',  { buffer = true })
         vim.keymap.set('n', '<leader>ic', '<cmd>MkdnTableNewColAfter<CR>',  { buffer = true })
         vim.keymap.set('n', '<leader>iC', '<cmd>MkdnTableNewColBefore<CR>', { buffer = true })
-
-        vim.keymap.set({'i', 's'}, '<Tab>', function()
-          return vim.fn['vsnip#available'](1) == 1 and '<Plug>(vsnip-expand-or-jump)' or '<cmd>MkdnTab<CR>'
-        end, { buffer = true, expr = true })
-
-        vim.keymap.set({'i', 's'}, '<S-Tab>', function()
-          return vim.fn['vsnip#jumpable'](-1) == 1 and '<Plug>(vsnip-jump-prev)' or '<cmd>MkdnSTab<CR>'
-        end, { buffer = true, expr = true })
       end
     })
   end,
--- .config/nvim/lua/plugins/vim-vsnip.lua
+++ .config/nvim/lua/plugins/vim-vsnip.lua
@@ -5,10 +5,23 @@ return {
   },
   config = function()
     vim.keymap.set({'i', 's'}, '<Tab>', function()
-      return vim.fn['vsnip#available'](1) == 1 and '<Plug>(vsnip-expand-or-jump)' or '<Tab>'
+      if vim.fn['vsnip#available'](1) == 1 then
+        return '<Plug>(vsnip-expand-or-jump)'
+      elseif vim.bo.filetype == 'markdown' then
+        return '<C-t>'
+      else
+        return '<Tab>'
+      end
     end, { expr = true })
+
     vim.keymap.set({'i', 's'}, '<S-Tab>', function()
-      return vim.fn['vsnip#jumpable'](-1) == 1 and '<Plug>(vsnip-jump-prev)' or '<S-Tab>'
+      if vim.fn['vsnip#jumpable'](-1) == 1 then
+        return '<Plug>(vsnip-jump-prev)'
+      elseif vim.bo.filetype == 'markdown' then
+        return '<C-d>'
+      else
+        return '<S-Tab>'
+      end
     end, { expr = true })
   end
 }
