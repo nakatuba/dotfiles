73c85a1fe7f50fbfe7e7e155977c6c63600910c2
nakatuba <nakatuba0626@gmail.com>
2023-08-18 17:06:26 +0900

    Revert "Install obsidian.nvim"
    This reverts commit 8530914943b42633e58d6e25f32c3c9b01664331.

--- .config/nvim/lua/plugins/obsidian.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'epwalsh/obsidian.nvim',
-  config = function()
-    require('obsidian').setup {
-      dir = '~/ghq/github.com/nakatuba/notes',
-      disable_frontmatter = true
-    }
-  end
-}
