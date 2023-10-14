26116b679a8d2aef725f6c9d9677a70b8cec9b7e
nakatuba <nakatuba0626@gmail.com>
2023-10-14 20:21:11 +0900

    Introduce copilot

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  'github/copilot.vim',
+  init = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+  config = function()
+    vim.keymap.set('i', '<C-f>', "copilot#Accept('<Right>')", { silent = true, expr = true, replace_keycodes = false })
+  end
+}
