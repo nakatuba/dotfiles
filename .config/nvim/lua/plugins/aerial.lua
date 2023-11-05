67e96b25e40bc8ad74002b71385c400ac5fbb96c
nakatuba <nakatuba0626@gmail.com>
2023-11-05 16:30:39 +0900

    Install aerial.nvim

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,24 @@
+return {
+  'stevearc/aerial.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons'
+  },
+  config = function()
+    require('aerial').setup {
+      layout = {
+        max_width = 0.9,
+        min_width = 30,
+        placement = 'edge',
+        preserve_equality = true
+      },
+      attach_mode = 'global',
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<C-k>', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        vim.keymap.set('n', '<C-j>', '<cmd>AerialNext<CR>', { buffer = bufnr })
+      end
+    }
+
+    vim.keymap.set('n', '<leader>o', '<cmd>AerialToggle!<CR>')
+  end
+}
