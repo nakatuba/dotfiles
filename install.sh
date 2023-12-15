e5bfaf8ab657147c30ca22283a1930e68cc03579
nakatuba <nakatuba0626@gmail.com>
2023-12-15 22:59:49 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+ln -s ~/dotfiles/.p10k.zsh ~
+ln -s ~/dotfiles/.tmux.conf ~
+ln -s ~/dotfiles/.zlogin ~
+ln -s ~/dotfiles/.zlogout ~
+ln -s ~/dotfiles/.zpreztorc ~
+ln -s ~/dotfiles/.zprofile ~
+ln -s ~/dotfiles/.zshenv ~
+ln -s ~/dotfiles/.zshrc ~
+
+mkdir -p ~/.config
+
+ln -s ~/dotfiles/.config/git ~/.config
+ln -s ~/dotfiles/.config/nvim ~/.config
+ln -s ~/dotfiles/.config/zsh ~/.config
