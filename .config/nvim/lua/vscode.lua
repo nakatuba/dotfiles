42236026c5724240ec2b63bd1a5dc05ea2353512
nakatuba <nakatuba0626@gmail.com>
2023-12-26 23:00:52 +0900

    Move vscode-neovim config

---
+++ .config/nvim/lua/vscode.lua
@@ -0,0 +1,33 @@
+require('lazy').setup {
+  { import = 'plugins.dial' },
+  { import = 'plugins.hop' },
+  { import = 'plugins.nvim-surround' },
+  { import = 'plugins.substitute' },
+  { import = 'plugins.treesj' },
+  { import = 'plugins.vim-commentary' },
+  { import = 'plugins.vim-operator-convert-case' },
+  { import = 'plugins.vim-swap' },
+  { import = 'plugins.vim-textobj-comment' },
+  { import = 'plugins.vim-textobj-entire' },
+  { import = 'plugins.vim-textobj-jsxattr' }
+}
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
+      vim.fn.setreg('*', vim.fn.getreg('"'))
+    end
+  end
+})
+
+vim.keymap.set('n', 'u',          function() vim.fn.VSCodeNotify('undo') end)
+vim.keymap.set('n', '<C-r>',      function() vim.fn.VSCodeNotify('redo') end)
+vim.keymap.set('n', 'gd',         function() vim.fn.VSCodeNotify('editor.action.revealDefinition') end)
+vim.keymap.set('n', 'gi',         function() vim.fn.VSCodeNotify('editor.action.goToImplementation') end)
+vim.keymap.set('n', 'gr',         function() vim.fn.VSCodeNotify('editor.action.goToReferences') end)
+vim.keymap.set('n', 'K',          function() vim.fn.VSCodeNotify('editor.action.showHover') end)
+vim.keymap.set('n', '<Leader>rn', function() vim.fn.VSCodeNotify('editor.action.rename') end)
+vim.keymap.set('n', '<Leader>ca', function() vim.fn.VSCodeNotify('editor.action.quickFix') end)
+vim.keymap.set('n', '<Leader>f',  function() vim.fn.VSCodeNotify('workbench.action.quickOpen') end)
+vim.keymap.set('n', '<Leader>b',  function() vim.fn.VSCodeNotify('workbench.action.showAllEditors') end)
+vim.keymap.set('n', '<Leader>e',  function() vim.fn.VSCodeNotify('workbench.view.explorer') end)
