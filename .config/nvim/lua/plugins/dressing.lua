df3104449ff86ebee3d6dcc507a7787c20a6b958
nakatuba <nakatuba0626@gmail.com>
2024-01-08 13:32:13 +0900

    Revert "Disable dressing.nvim in nvim-tree"
    This reverts commit 7ae19c2bdedbc302a1dcb24cd98ea1677a7fb5de.

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -6,14 +6,7 @@ return {
         win_options = {
           winblend = 0,
           winhighlight = 'NormalFloat:Normal,FloatBorder:Blue'
-        },
-        get_config = function()
-          if vim.bo.filetype == 'NvimTree' then
-            return {
-              enabled = false
-            }
-          end
-        end
+        }
       },
       select = {
         get_config = function(opts)
