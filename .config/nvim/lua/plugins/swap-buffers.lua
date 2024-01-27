9220c864b8751b742a8308012993b12c1db2a77b
nakatuba <nakatuba0626@gmail.com>
2024-01-28 06:07:37 +0900

    Minor changes

--- .config/nvim/lua/plugins/swap-buffers.lua
+++ .config/nvim/lua/plugins/swap-buffers.lua
@@ -1,9 +1,9 @@
 return {
   'caenrique/swap-buffers.nvim',
   config = function()
-    vim.keymap.set('n', '<M-h>', "<cmd>lua require('swap-buffers').swap_buffers('h')<CR>")
-    vim.keymap.set('n', '<M-j>', "<cmd>lua require('swap-buffers').swap_buffers('j')<CR>")
-    vim.keymap.set('n', '<M-k>', "<cmd>lua require('swap-buffers').swap_buffers('k')<CR>")
-    vim.keymap.set('n', '<M-l>', "<cmd>lua require('swap-buffers').swap_buffers('l')<CR>")
+    vim.keymap.set('n', '<M-h>', function() require('swap-buffers').swap_buffers('h') end)
+    vim.keymap.set('n', '<M-j>', function() require('swap-buffers').swap_buffers('j') end)
+    vim.keymap.set('n', '<M-k>', function() require('swap-buffers').swap_buffers('k') end)
+    vim.keymap.set('n', '<M-l>', function() require('swap-buffers').swap_buffers('l') end)
   end
 }
