e9afa30a7aaf1c538df0e8e3c96bb93cde7001c5
nakatuba <nakatuba0626@gmail.com>
2024-03-31 21:19:56 +0900

    Update notes.nvim config

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -15,10 +15,12 @@ return {
     vim.keymap.set('n', '<leader>nn', require('notes').new)
     vim.keymap.set('n', '<leader>no', require('notes').open)
 
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'markdown',
+    vim.api.nvim_create_autocmd('BufEnter', {
       callback = function()
-        vim.keymap.set('i', '[[', require('notes').insert_link)
+        if vim.fn.getcwd() == vim.fn.resolve(vim.fn.expand('~/Dropbox/notes')) then
+          vim.keymap.set('n', '<leader>f', require('notes').open,        { buffer = true })
+          vim.keymap.set('i', '[[',        require('notes').insert_link, { buffer = true })
+        end
       end
     })
   end
