b20fa06aa8db95b682cdb4b4805372db796c48cf
nakatuba <nakatuba0626@gmail.com>
2024-04-08 02:51:00 +0900

    Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,3 +1,12 @@
+local wezterm = require('wezterm')
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+  end
+)
+
 return {
   font_size = 10.0,
   warn_about_missing_glyphs = false,
