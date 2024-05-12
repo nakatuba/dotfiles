1676100c51b50f5d9a01c41b552d0fa710e99780
nakatuba <nakatuba0626@gmail.com>
2024-05-13 00:40:37 +0900

    Update fzf-git.zsh

--- .config/zsh/fzf-git.zsh
+++ .config/zsh/fzf-git.zsh
@@ -10,7 +10,7 @@ _gf() {
   is_in_git_repo || return
   git -c color.status=always status --short |
   fzf-down -m --ansi --nth 2..,.. \
-    --preview 'git diff --color=always -- {-1} | sed 1,4d' |
+    --preview 'git diff --color=always -- {-1} | sed 1,4d; bat --color=always {-1}' |
   cut -c4- | sed 's/.* -> //'
 }
 
