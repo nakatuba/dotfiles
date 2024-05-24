cfe9356e5e5b25f2779b7093229468ac29464f3b
nakatuba <nakatuba0626@gmail.com>
2024-05-24 10:13:01 +0900

    Show stackline icons

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,7 +1,7 @@
 local stackline = require('stackline')
-local config = require('stackline.conf')
 
-config.paths.yabai = '/opt/homebrew/bin/yabai'
-config.appearance.showIcons = false
-
-stackline:init(config)
+stackline:init {
+  paths = {
+    yabai = '/opt/homebrew/bin/yabai'
+  }
+}
