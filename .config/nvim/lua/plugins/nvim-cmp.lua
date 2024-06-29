d3fb2ce7038d84aa4158f11ede9a15456ab71a10
nakatuba <nakatuba0626@gmail.com>
2024-06-30 01:21:41 +0900

    Remove neovim snippet completion

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,7 +4,6 @@ return {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
-    'hrsh7th/cmp-vsnip',
     'hrsh7th/vim-vsnip'
   },
   config = function()
@@ -32,8 +31,7 @@ return {
       sources = {
         { name = 'nvim_lsp' },
         { name = 'buffer' },
-        { name = 'path' },
-        { name = 'vsnip' }
+        { name = 'path' }
       }
     }
   end
