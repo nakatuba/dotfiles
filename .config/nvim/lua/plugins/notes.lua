60a0c0ca03eb2f7b89de4d90b967f2c30472a56b
nakatuba <nakatuba0626@gmail.com>
2024-07-07 03:41:01 +0900

    Change notes.nvim mappings

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -15,17 +15,24 @@ return {
     vim.keymap.set('n', '<leader>nn', require('notes').new_note)
     vim.keymap.set('n', '<leader>no', require('notes').open_note)
     vim.keymap.set('n', '<leader>nd', require('notes').open_daily_note)
-    vim.keymap.set('n', '<leader>ng', function()
-      require('telescope.builtin').live_grep {
-        cwd = require('notes').config.dir
-      }
-    end)
 
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'markdown',
       callback = function()
         if vim.startswith(vim.fn.expand('%:p'), vim.fn.expand(require('notes').config.dir)) then
-          vim.keymap.set('n', '<leader>il', require('notes').insert_link, { buffer = true })
+          vim.keymap.set('i', '[[', require('notes').insert_link, { buffer = true })
+
+          vim.keymap.set('n', '<leader>g', function()
+            require('telescope.builtin').live_grep {
+              cwd = require('notes').config.dir,
+            }
+          end, { buffer = true })
+
+          vim.keymap.set('n', '<C-g><C-h>', function()
+            require('telescope.builtin').git_bcommits {
+              cwd = require('notes').config.dir,
+            }
+          end, { buffer = true })
         end
       end
     })
