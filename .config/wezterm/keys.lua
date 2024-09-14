fffc6b10447e5210e78909747f7c350a8c4f190e
nakatuba <nakatuba0626@gmail.com>
2024-09-15 02:15:52 +0900

    Add wezterm keybinding to toggle opacity

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -1,6 +1,21 @@
 local wezterm = require('wezterm')
 
+wezterm.on('toggle-opacity', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.window_background_opacity then
+    overrides.window_background_opacity = 0.8
+  else
+    overrides.window_background_opacity = nil
+  end
+  window:set_config_overrides(overrides)
+end)
+
 return {
+  {
+    key = 'u',
+    mods = 'SUPER',
+    action = wezterm.action.EmitEvent('toggle-opacity')
+  },
   {
     key = '/',
     mods = 'CTRL',
