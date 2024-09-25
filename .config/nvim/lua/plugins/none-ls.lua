bfe78f1e3de9ba4a068e1065d2f7d742d203a797
nakatuba <nakatuba0626@gmail.com>
2024-09-25 11:50:27 +0900

    Update cspell.nvim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,30 +5,47 @@ return {
     'davidmh/cspell.nvim'
   },
   config = function()
-    local null_ls = require('null-ls')
+    local cspell_config = {
+      cspell_config_dirs = {
+        '~/.config/cspell',
+        '~/.local/share/cspell'
+      }
+    }
 
-    null_ls.setup {
+    require('null-ls').setup {
       diagnostics_format = '[#{c}] #{m} (#{s})',
       on_attach = function(client, bufnr)
+        vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',                   { buffer = bufnr })
+        vim.keymap.set('n', '<leader>d',  '<cmd>Telescope diagnostics<CR>',                           { buffer = bufnr })
+        vim.keymap.set('n', '[d',         '<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>', { buffer = bufnr })
+        vim.keymap.set('n', ']d',         '<cmd>lua vim.diagnostic.goto_next({ float = false })<CR>', { buffer = bufnr })
+
         if client.supports_method('textDocument/formatting') then
           vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = bufnr,
             callback = function() vim.lsp.buf.format() end
           })
         end
+
+        vim.api.nvim_create_autocmd('CursorHold', {
+          buffer = bufnr,
+          callback = function() vim.diagnostic.open_float(nil, { focusable = false, scope = 'cursor' }) end
+        })
       end,
       sources = {
         require('cspell').diagnostics.with {
-          extra_args = { '--config', '~/.config/cspell/cspell.json' },
+          config = cspell_config,
           diagnostics_postprocess = function(diagnostic)
             diagnostic.severity = vim.diagnostic.severity.HINT
           end
         },
-        require('cspell').code_actions,
+        require('cspell').code_actions.with {
+          config = cspell_config
+        },
 
         -- javascript
         require('none-ls.diagnostics.eslint'),
-        null_ls.builtins.formatting.prettier.with {
+        require('null-ls').builtins.formatting.prettier.with {
           disabled_filetypes = { 'json', 'yaml' }
         },
 
@@ -39,12 +56,12 @@ return {
             return vim.fn.fnamemodify(params.bufname, ':h')
           end
         },
-        null_ls.builtins.formatting.black.with {
+        require('null-ls').builtins.formatting.black.with {
           cwd = function(params)
             return vim.fn.fnamemodify(params.bufname, ':h')
           end
         },
-        null_ls.builtins.formatting.isort.with {
+        require('null-ls').builtins.formatting.isort.with {
           cwd = function(params)
             return vim.fn.fnamemodify(params.bufname, ':h')
           end
