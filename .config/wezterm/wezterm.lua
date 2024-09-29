9282f80324da2f48c9495bebc83858eb5d0f4eaf
nakatuba <nakatuba0626@gmail.com>
2024-09-29 22:22:33 +0900

    Set yabai sub-layer

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 wezterm.on('gui-startup', function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
+  window:gui_window():maximize()
 end)
 
 config.font = wezterm.font('MesloLGS NF')
