072a7c569227a3c3f7d851427e6c4c100c33e57f
nakatuba <nakatuba0626@gmail.com>
2024-10-12 20:24:12 +0900

    neovim: Minor changes

--- .config/nvim/lua/plugins/nvim-pasta.lua
+++ .config/nvim/lua/plugins/nvim-pasta.lua
@@ -3,5 +3,7 @@ return {
   config = function()
     vim.keymap.set({'n', 'x'}, 'p', require('pasta.mapping').p)
     vim.keymap.set({'n', 'x'}, 'P', require('pasta.mapping').P)
+
+    require('pasta').config.indent_fix = false
   end
 }
