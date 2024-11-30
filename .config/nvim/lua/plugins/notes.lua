0bdb23df9b5a571cb4f683855cd3a4d4340a5c6b
nakatuba <nakatuba0626@gmail.com>
2024-12-01 05:40:49 +0900

    Remove daily notes

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -11,25 +11,11 @@ return {
   },
   config = function()
     require('notes').setup {
-      dir = '~/Dropbox/Apps/remotely-save/notes',
-      daily_notes = {
-        dir = '~/Dropbox/Apps/remotely-save/notes/daily'
-      }
+      dir = '~/Dropbox/Apps/remotely-save/notes'
     }
 
-    vim.keymap.set('n', '<leader>nn', function()
-      vim.ui.select({ 'New note', 'New daily note' }, {
-        prompt = 'Select note to create',
-      }, function(choice)
-        if choice == 'New note' then
-          require('notes').new_note()
-        elseif choice == 'New daily note' then
-          require('notes').new_daily_note()
-        end
-      end)
-    end)
+    vim.keymap.set('n', '<leader>nn', require('notes').new_note)
     vim.keymap.set('n', '<leader>no', require('notes').open_note)
-    vim.keymap.set('n', '<leader>nd', require('notes').open_daily_note)
 
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'markdown',
