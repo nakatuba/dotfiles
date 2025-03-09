c411651e64f4b4c2ab80bb0e8d76963ef6eecfbc
nakatuba <nakatuba0626@gmail.com>
2025-03-09 17:16:56 +0900

    wezterm: Disable fullscreen on startup

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -4,7 +4,7 @@ local wezterm = require('wezterm')
 wezterm.on('toggle-opacity', function(window, pane)
   local overrides = window:get_config_overrides() or {}
   if not overrides.window_background_opacity then
-    overrides.window_background_opacity = 1.0
+    overrides.window_background_opacity = 0.8
   else
     overrides.window_background_opacity = nil
   end
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,9 @@
 local wezterm = require('wezterm')
-local mux = wezterm.mux
 local config = wezterm.config_builder()
-
-wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
-end)
 
 config.font = wezterm.font('MesloLGS NF')
 config.hide_tab_bar_if_only_one_tab = true
 config.keys = require('keybinds').keys
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
-config.window_background_opacity = 0.8
 
 return config
