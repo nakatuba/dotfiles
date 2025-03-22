2d8885388d92798c449e89d8c45271c276adc70c
nakatuba <nakatuba0626@gmail.com>
2025-03-23 05:43:29 +0900

    Update dressing.nvim

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -3,9 +3,16 @@ return {
   config = function()
     require('dressing').setup {
       input = {
+        title_pos = 'center',
+        relative = 'editor',
         win_options = {
           winblend = 0,
           winhighlight = 'NormalFloat:Normal,FloatBorder:Blue,FloatTitle:Title'
+        },
+        mappings = {
+          i = {
+            ['<Esc>'] = 'Close'
+          }
         }
       },
       select = {
