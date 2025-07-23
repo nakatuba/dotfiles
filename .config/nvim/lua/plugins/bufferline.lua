f312faa28d78e464e04e361ea6a3cd22f70a7e02
nakatuba <nakatuba0626@gmail.com>
2025-07-24 00:41:46 +0900

    Revert "neovim: Add tab mappings"
    This reverts commit 405102ea5dd0ef113d497c5faa361bba2e7f1a68.

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -22,27 +22,5 @@ return {
     vim.keymap.set('n', '<C-n>', '<cmd>BufferLineCycleNext<CR>')
     vim.keymap.set('n', '<M-p>', '<cmd>BufferLineMovePrev<CR>')
     vim.keymap.set('n', '<M-n>', '<cmd>BufferLineMoveNext<CR>')
-
-    vim.keymap.set('n', 'sp', function() vim.cmd.tabprev() end)
-    vim.keymap.set('n', 'sn', function() vim.cmd.tabnext() end)
-    vim.keymap.set('n', 'sP', function()
-      if vim.fn.tabpagenr() == 1 then
-        vim.cmd.tabmove()
-      else
-        vim.cmd.tabmove('-')
-      end
-    end)
-    vim.keymap.set('n', 'sN', function()
-      if vim.fn.tabpagenr() == vim.fn.tabpagenr('$') then
-        vim.cmd.tabmove('0')
-      else
-        vim.cmd.tabmove('+')
-      end
-    end)
-    vim.keymap.set('n', 's,', function()
-      vim.ui.input({ prompt = 'Rename tab to: ' }, function(input)
-        require('bufferline').rename_tab { input }
-      end)
-    end)
   end
 }
