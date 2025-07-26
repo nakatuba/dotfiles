e02a6555168bc7bcf331f0d89e578065c5c7b83e
nakatuba <nakatuba0626@gmail.com>
2025-07-27 01:39:04 +0900

    Enable mise completion

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -2,6 +2,8 @@ autoload bashcompinit && bashcompinit
 
 command -v brew > /dev/null && FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
 
+command -v mise > /dev/null && eval "$(mise completion zsh)"
+
 command -v pip > /dev/null && eval "$(pip completion --zsh)"
 
 command -v terraform > /dev/null && complete -o nospace -C $(which terraform) terraform
