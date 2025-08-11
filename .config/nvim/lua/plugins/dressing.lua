3260c60efd68878527e43271256ef049e1544cc2
nakatuba <nakatuba0626@gmail.com>
2025-08-11 23:59:27 +0900

    Use dressing.nvim input

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -6,7 +6,6 @@ return {
         title_pos = 'center',
         relative = 'editor',
         win_options = {
-          winblend = 0,
           winhighlight = 'NormalFloat:Normal,FloatBorder:Blue,FloatTitle:Title'
         },
         mappings = {
@@ -14,23 +13,6 @@ return {
             ['<Esc>'] = 'Close'
           }
         }
-      },
-      select = {
-        get_config = function(opts)
-          if opts.kind == 'codeaction' then
-            return {
-              backend = 'builtin',
-              builtin = {
-                relative = 'cursor',
-                win_options = {
-                  winblend = 0,
-                  winhighlight = 'NormalFloat:Normal,FloatBorder:Blue,FloatTitle:Title'
-                },
-                min_height = 0
-              }
-            }
-          end
-        end
       }
     }
   end
