a925e632e374a1e288b8f583094fe390215498cd
nakatuba <nakatuba0626@gmail.com>
2025-08-13 13:55:35 +0900

    Uninstall nvim-pasta

--- .config/nvim/lua/plugins/nvim-pasta.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'hrsh7th/nvim-pasta',
-  config = function()
-    vim.keymap.set({'n', 'x'}, 'p', require('pasta.mapping').p)
-    vim.keymap.set({'n', 'x'}, 'P', require('pasta.mapping').P)
-
-    require('pasta').config.indent_fix = false
-  end
-}
