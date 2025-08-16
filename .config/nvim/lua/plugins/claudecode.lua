165f7e959dc65b215b212589647190448a1cd346
nakatuba <nakatuba0626@gmail.com>
2025-08-17 02:16:46 +0900

    Update claudecode.nvim

--- .config/nvim/lua/plugins/claudecode.lua
+++ .config/nvim/lua/plugins/claudecode.lua
@@ -6,15 +6,17 @@ return {
   config = function()
     require('claudecode').setup {
       terminal = {
-        snacks_win_opts = {
-          keys = {
-            claude_hide = { '<M-Esc>', function(self) self:hide() end, mode = 't' }
-          }
+        provider = 'external',
+        provider_opts = {
+          external_terminal_cmd = 'tmux split-window -h %s'
         }
       }
     }
 
-    vim.keymap.set('n', '<M-Esc>', '<cmd>ClaudeCodeFocus<CR>')
-    vim.keymap.set('x', '<M-Esc>', '<cmd>ClaudeCodeSend<CR>')
+    vim.keymap.set('n', '<leader>af', '<cmd>ClaudeCodeFocus<CR>')
+    vim.keymap.set('n', '<leader>ab', '<cmd>ClaudeCodeAdd %<CR>')
+    vim.keymap.set('x', '<leader>as', '<cmd>ClaudeCodeSend<CR>')
+    vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<CR>')
+    vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<CR>')
   end
 }
