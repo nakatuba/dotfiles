9d2d14ad147bc4947893b4b02a1071c0f2446aad
nakatuba <nakatuba0626@gmail.com>
2025-12-16 07:32:18 +0900

    feat(emacs): update evil config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,5 +4,11 @@
 
 (use-package evil
   :ensure t
+  :init
+  (setq evil-split-window-below t)
+  (setq evil-vsplit-window-right t)
+  (setq evil-want-C-h-delete t)
+  (setq evil-want-C-u-delete t)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (define-key evil-normal-state-map (kbd "s") 'evil-window-map))
