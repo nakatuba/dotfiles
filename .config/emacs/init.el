6b767f398605cac2d89a64801e43f06f8f9c7e60
nakatuba <nakatuba0626@gmail.com>
2025-12-16 09:15:36 +0900

    feat(emacs): update evil config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,10 +5,12 @@
 (use-package evil
   :ensure t
   :init
+  (setq evil-disable-insert-state-bindings t)
   (setq evil-split-window-below t)
   (setq evil-vsplit-window-right t)
-  (setq evil-want-C-h-delete t)
-  (setq evil-want-C-u-delete t)
   :config
   (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (define-key evil-insert-state-map (kbd "C-u") 'evil-delete-back-to-indentation)
+  (define-key evil-insert-state-map (kbd "C-w") 'evil-delete-backward-word)
   (define-key evil-normal-state-map (kbd "s") 'evil-window-map))
