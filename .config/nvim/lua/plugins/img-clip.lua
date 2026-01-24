44da716b6a619069b26e43ae96e888c2d9287d6c
nakatuba <nakatuba0626@gmail.com>
2026-01-24 23:12:25 +0900

    feat(neovim): update img-clip.nvim config

--- .config/nvim/lua/plugins/img-clip.lua
+++ .config/nvim/lua/plugins/img-clip.lua
@@ -4,7 +4,6 @@ return {
     require('img-clip').setup {
       default = {
         file_name = '%Y%m%d%H%M%S',
-        relative_to_current_file = true,
         insert_mode_after_paste = false,
         prompt_for_file_name = false
       }
