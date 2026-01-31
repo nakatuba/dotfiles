a40fbc2119774256ecaab522daa9d46a030a5e69
nakatuba <nakatuba0626@gmail.com>
2026-02-01 01:16:29 +0900

    refactor(neovim): use sonokai get_palette

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -18,17 +18,20 @@ return {
     }
 
     if vim.g.colors_name == 'sonokai' then
-      vim.api.nvim_set_hl(0, '@markup.heading',             { link = 'Normal' })
-      vim.api.nvim_set_hl(0, '@markup.quote',               { link = 'Normal' })
-      vim.api.nvim_set_hl(0, '@markup.raw.block.markdown',  { link = 'Normal' })
-      vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { link = 'Orange' })
+      local config = vim.fn['sonokai#get_configuration']()
+      local palette = vim.fn['sonokai#get_palette'](config.style, config.colors_override)
 
-      vim.api.nvim_set_hl(0, 'RenderMarkdownH1Bg', { fg = '#fc5d7c', underline = true })
-      vim.api.nvim_set_hl(0, 'RenderMarkdownH2Bg', { fg = '#f39660', underline = true })
-      vim.api.nvim_set_hl(0, 'RenderMarkdownH3Bg', { fg = '#e7c664', underline = true })
-      vim.api.nvim_set_hl(0, 'RenderMarkdownH4Bg', { fg = '#9ed072', underline = true })
-      vim.api.nvim_set_hl(0, 'RenderMarkdownH5Bg', { fg = '#76cce0', underline = true })
-      vim.api.nvim_set_hl(0, 'RenderMarkdownH6Bg', { fg = '#b39df3', underline = true })
+      vim.api.nvim_set_hl(0, '@markup.heading',             { fg = palette.fg[1] })
+      vim.api.nvim_set_hl(0, '@markup.quote',               { fg = palette.fg[1] })
+      vim.api.nvim_set_hl(0, '@markup.raw.block.markdown',  { fg = palette.fg[1] })
+      vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { fg = palette.orange[1] })
+
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH1Bg', { fg = palette.red[1],    underline = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH2Bg', { fg = palette.orange[1], underline = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH3Bg', { fg = palette.yellow[1], underline = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH4Bg', { fg = palette.green[1],  underline = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH5Bg', { fg = palette.blue[1],   underline = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH6Bg', { fg = palette.purple[1], underline = true })
     end
   end,
   ft = { 'markdown' }
