5f56bfd46cb72b0f7e0b37d8c0f668c493538bb1
nakatuba <nakatuba0626@gmail.com>
2026-02-01 19:50:40 +0900

    feat(ghostty): set font-family for Japanese

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('MesloLGS NF')
+config.font = wezterm.font_with_fallback { 'MesloLGS NF', 'Noto Sans CJK JP' }
 config.hide_tab_bar_if_only_one_tab = true
 config.keys = require('keybinds').keys
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
