92bdf40fbb022504c8ad6251da47dd35a43406bc
nakatuba <nakatuba0626@gmail.com>
2026-02-19 21:28:16 +0900

    fix(neovim): resolve error with `:wqa`

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,3 +3,13 @@ vim.api.nvim_create_autocmd('FocusGained', {
     vim.cmd.checktime()
   end
 })
+
+vim.api.nvim_create_autocmd('ExitPre', {
+  callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[buf].buftype == 'terminal' then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end
+  end
+})
