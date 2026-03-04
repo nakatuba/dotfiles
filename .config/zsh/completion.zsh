4fcfc9845731b56c58ef9455e781d16a576061b6
nakatuba <nakatuba0626@gmail.com>
2026-03-04 12:44:19 +0900

    feat(mise): install gwq

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -9,3 +9,5 @@ command -v pip > /dev/null && eval "$(pip completion --zsh)"
 command -v terraform > /dev/null && complete -o nospace -C $(which terraform) terraform
 
 autoload -Uz compinit && compinit
+
+command -v gwq > /dev/null && source <(gwq completion zsh)
