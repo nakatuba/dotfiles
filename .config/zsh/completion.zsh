78acf1c3634907e4a3a1fe70fd13e153be13212a
nakatuba <nakatuba0626@gmail.com>
2026-05-03 16:04:11 +0900

    fix(zsh): move mise completion after compinit to fix completion not working

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,13 +1,10 @@
-autoload bashcompinit && bashcompinit
+autoload -Uz compinit && compinit
+autoload -Uz bashcompinit && bashcompinit
 
-command -v brew > /dev/null && FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
+command -v gwq > /dev/null && eval "$(gwq completion zsh)"
 
 command -v mise > /dev/null && eval "$(mise completion zsh)"
 
 command -v pip > /dev/null && eval "$(pip completion --zsh)"
 
 command -v terraform > /dev/null && complete -o nospace -C $(which terraform) terraform
-
-autoload -Uz compinit && compinit
-
-command -v gwq > /dev/null && source <(gwq completion zsh)
